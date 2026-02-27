9f06e39320b48215d011d188fba5c09e05bf896c
cubito1080 <jerov79@gmail.com>
2026-02-27 11:09:06 -0500

    🔧 add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { ContinentModule } from './continent/continent.module';
 import { CityModule } from './city/city.module';
 import { CountryModule } from './country/country.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ContinentModule, CityModule, CountryModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'jero',
+      password: '123',
+      database: 'geography_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ContinentModule,
+    CityModule,
+    CountryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

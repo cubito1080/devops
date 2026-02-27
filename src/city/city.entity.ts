85d499dc29e74b879ccebdbdea84943a96bae6eb
cubito1080 <jerov79@gmail.com>
2026-02-27 12:11:07 -0500

    ✨ add city entity

---
+++ src/city/city.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class City {
+  @PrimaryGeneratedColumn()
+  city_id: number;
+
+  @Column()
+  city_name: string;
+
+  @Column('jsonb')
+  economical_index: Record<string, number>;
+
+  @Column('jsonb')
+  languages: string[];
+
+  @Column()
+  population: number;
+
+  @Column()
+  net_area: number;
+}

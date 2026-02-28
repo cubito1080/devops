8d849d40406ef37099708a33938a56d0ff7df180
cubito1080 <jerov79@gmail.com>
2026-02-28 10:39:09 -0500

    ✨ add create continent

--- src/continent/continent.service.ts
+++ src/continent/continent.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Continent } from './continent.entity';
+import { CreateContinentDto } from './dto/create-continent.dto';
 
 @Injectable()
 export class ContinentService {
@@ -9,4 +10,19 @@ export class ContinentService {
     @InjectRepository(Continent)
     private continentRepository: Repository<Continent>,
   ) {}
+
+  async create(dto: CreateContinentDto): Promise<Continent> {
+    const checker = await this.continentRepository.findOne({
+      where: { name: dto.name },
+    });
+
+    if (checker) {
+      throw new ConflictException(
+        \Continent with this \ already exists\,
+      );
+    }
+    const continent = this.continentRepository.create(dto);
+
+    return await this.continentRepository.save(continent);
+  }
 }

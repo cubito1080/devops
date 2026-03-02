57a75436956f515d79f1a0b6ba658f12e69779c6
cubito1080 <jerov79@gmail.com>
2026-03-01 20:24:46 -0500

    ✨ add create country route

--- src/country/country.controller.ts
+++ src/country/country.controller.ts
@@ -1,16 +1,19 @@
-import {
+﻿import {
   Controller,
-  Get,
   Post,
-  Put,
-  Delete,
   Body,
-  Param,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { CountryService } from './country.service';
 import { Country } from './country.entity';
+import { CreateCountryDto } from './dto/create-continent.dto';
 
 @Controller('countries')
 export class CountryController {
   constructor(private readonly countryService: CountryService) {}
+
+  @Post()
+  create(@Body() dto: CreateCountryDto): Promise<Country> {
+    return this.countryService.create(dto);
+  }
 }

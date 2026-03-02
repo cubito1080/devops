e9e23c50b9b86902a8912ccd32f988111468c248
cubito1080 <jerov79@gmail.com>
2026-03-01 20:26:15 -0500

    ✨ add create city route

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,16 +1,18 @@
-import {
+﻿import {
   Controller,
-  Get,
   Post,
-  Put,
-  Delete,
   Body,
-  Param,
 } from '@nestjs/common';
 import { CityService } from './city.service';
 import { City } from './city.entity';
+import { CreateCityDto } from './dto/create-city.dto';
 
 @Controller('cities')
 export class CityController {
   constructor(private readonly cityService: CityService) {}
+
+  @Post()
+  create(@Body() dto: CreateCityDto): Promise<City> {
+    return this.cityService.create(dto);
+  }
 }

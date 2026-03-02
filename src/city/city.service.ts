1991324629ba10e6638f261796e0252916ee8eef
cubito1080 <jerov79@gmail.com>
2026-03-01 20:06:32 -0500

    ✨ add create city

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,12 +1,47 @@
-import { Injectable } from '@nestjs/common';
+﻿import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { City } from './city.entity';
+import { CreateCityDto } from './dto/create-city.dto';
+import { Country } from '../country/country.entity';
 
 @Injectable()
 export class CityService {
   constructor(
     @InjectRepository(City)
     private cityRepository: Repository<City>,
+
+    @InjectRepository(Country)
+    private countryRepository: Repository<Country>,
   ) {}
+
+  async create(dto: CreateCityDto): Promise<City> {
+    const country = await this.countryRepository.findOne({
+      where: { country_id: dto.country_id },
+    });
+
+    if (!country) {
+      throw new NotFoundException(
+        `Country with id ${dto.country_id} does not exist`,
+      );
+    }
+    const existingCity = await this.cityRepository.findOne({
+      where: { city_name: dto.city_name },
+    });
+
+    if (existingCity) {
+      throw new ConflictException(
+        `City with name "${dto.city_name}" already exists`,
+      );
+    }
+    const city = this.cityRepository.create({
+      ...dto,
+      country,
+    });
+    return await this.cityRepository.save(city);
+  }
 }

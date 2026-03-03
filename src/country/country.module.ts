aa4052f4a4d933acfb6ef83f84b6b3702505bad9
cubito1080 <jerov79@gmail.com>
2026-03-02 19:45:10 -0500

    🔧 wire up CountryModule with controller and service

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Country } from './country.entity';
+import { CountryController } from './country.controller';
+import { CountryService } from './country.service';
+import { Continent } from '../continent/continent.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Country, Continent])],
+  controllers: [CountryController],
+  providers: [CountryService],
+})
 export class CountryModule {}

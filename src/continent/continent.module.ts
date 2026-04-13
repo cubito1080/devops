7a5076466bcd62ce49a1e9331b3d89baf61e583a
cubito1080 <jerov79@gmail.com>
2026-04-13 15:52:25 -0500

    :recycle: refactor(continent): export service and register v2 controller in module

--- src/continent/continent.module.ts
+++ src/continent/continent.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Continent } from './continent.entity';
 import { ContinentController } from './continent.controller';
+import { ContinentV2Controller } from './continent.v2.controller';
 import { ContinentService } from './continent.service';
-//import { CannotGetEntityManagerNotConnectedError } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Continent])],
-  controllers: [ContinentController],
+  controllers: [ContinentController, ContinentV2Controller],
   providers: [ContinentService],
+  exports: [ContinentService],
 })
 export class ContinentModule {}

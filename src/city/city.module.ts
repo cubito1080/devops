11a5b3de6ef44f26b658a5efa7751047202c6607
cubito1080 <jerov79@gmail.com>
2026-04-13 15:52:52 -0500

    :recycle: refactor(city): export service and register v2 controller in module

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { City } from './city.entity';
 import { Country } from '../country/country.entity';
 import { CityController } from './city.controller';
+import { CityV2Controller } from './city.v2.controller';
 import { CityService } from './city.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([City, Country])],
-  controllers: [CityController],
+  controllers: [CityController, CityV2Controller],
   providers: [CityService],
+  exports: [CityService],
 })
 export class CityModule {}
